4bed40690741bd072257b950c26a0ecefc59156f
Yan Chubikov <yan.gyunashyan@gmail.com>
2021-07-10 11:42:19 +0200

    [2.0] Direction property range fix
    Signed-off-by: Yan Chubikov <yan.gyunashyan@gmail.com>

--- src/main/java/net/oskarstrom/dashloader/blockstate/property/DashDirectionProperty.java
+++ src/main/java/net/oskarstrom/dashloader/blockstate/property/DashDirectionProperty.java
@@ -2,6 +2,7 @@ package net.oskarstrom.dashloader.blockstate.property;
 
 import io.activej.serializer.annotations.Deserialize;
 import io.activej.serializer.annotations.Serialize;
+import io.activej.serializer.annotations.SerializeNullable;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.math.Direction;
 import net.oskarstrom.dashloader.DashRegistry;
@@ -9,27 +10,46 @@ import net.oskarstrom.dashloader.api.annotation.DashConstructor;
 import net.oskarstrom.dashloader.api.annotation.DashObject;
 import net.oskarstrom.dashloader.api.enums.ConstructorMode;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @DashObject(DirectionProperty.class)
 public class DashDirectionProperty implements DashProperty {
-
-
     @Serialize(order = 0)
     public final String name;
 
-    public DashDirectionProperty(@Deserialize("name") String name) {
+    @Serialize(order = 1)
+    @SerializeNullable
+    public final Direction[] directions;
+
+
+    public DashDirectionProperty(@Deserialize("name") String name,
+                                 @Deserialize("directions") Direction[] directions) {
         this.name = name;
+        this.directions = directions;
     }
 
     @DashConstructor(ConstructorMode.OBJECT)
     public DashDirectionProperty(DirectionProperty property) {
         name = property.getName();
+        final Collection<Direction> values = property.getValues();
+        final int size = values.size();
+        if (size == 6) {
+            this.directions = null;
+        } else {
+            List<Direction> directionsOut = new ArrayList<>(values);
+            this.directions = new Direction[directionsOut.size()];
+            for (int i = 0; i < directionsOut.size(); i++) {
+                this.directions[i] = directionsOut.get(i);
+            }
+        }
     }
 
     @Override
     public DirectionProperty toUndash(DashRegistry registry) {
-        return DirectionProperty.of(name, Direction.values());
+        return DirectionProperty.of(name, directions == null ? Direction.values() : directions);
     }
 
     @Override
